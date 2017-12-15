a1291d27eb413487b60975c75423a8bee8c5bf59
Chupurnov Valeriy <skoder@ya.ru>
2017-12-15 16:51:04 +0500

    New version 3.1.3. Read more https://github.com/xdan/jodit/releases/tag/3.1.3

---
+++ src/langs/index.ts
@@ -0,0 +1,10 @@
+/*!
+ * Jodit Editor (https://xdsoft.net/jodit/)
+ * License https://xdsoft.net/jodit/license.html
+ * Copyright 2013-2017 Valeriy Chupurnov xdsoft.net
+ */
+
+export {default as de} from './de';
+export {default as en} from './en';
+export {default as fr} from './fr';
+export {default as ru} from './ru';
