c75139d1c573ebf2046d357bc8a2b7755f2c1dbc
Chupurnov Valeriy <skoder@ya.ru>
2018-02-02 14:11:35 +0500

    Added examples and added arabic language

--- src/langs/index.ts
+++ src/langs/index.ts
@@ -8,3 +8,4 @@ export {default as de} from './de';
 export {default as en} from './en';
 export {default as fr} from './fr';
 export {default as ru} from './ru';
+export {default as ar} from './ar';
