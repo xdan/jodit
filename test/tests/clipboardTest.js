58009faf4d6ba8e58d7f4a4205dedb20c651e777
Valera <v-chupurnov@edadeal.ru>
2018-12-04 02:17:11 +0300

    Added pretty command

--- test/tests/clipboardTest.js
+++ test/tests/clipboardTest.js
@@ -1,18 +1,18 @@
-describe('Clipboard text', function () {
-    describe('Paste HTML', function () {
-        it('Should show paste html dialog', function () {
+describe('Clipboard text', function() {
+    describe('Paste HTML', function() {
+        it('Should show paste html dialog', function() {
             var editor = new Jodit(appendTestArea(), {
                 defaultActionOnPaste: Jodit.INSERT_AS_HTML,
             });
 
             var pastedText = '<p>test</p>';
 
-            var emulatePasteEvent = function (data) {
+            var emulatePasteEvent = function(data) {
                 data.clipboardData = {
                     types: ['text/html'],
-                    getData: function (type) {
+                    getData: function(type) {
                         return pastedText;
-                    }
+                    },
                 };
             };
 
@@ -20,27 +20,37 @@ describe('Clipboard text', function () {
 
             expect(editor.value).to.be.equal('');
 
-            var dialog = editor.ownerDocument.querySelector('.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id='  + editor.id + ']')
+            var dialog = editor.ownerDocument.querySelector(
+                '.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id=' +
+                    editor.id +
+                    ']'
+            );
             expect(dialog).to.be.not.equal(null);
         });
-        describe('Prevent show dialog', function () {
-            it('Should not show paste html dialog if beforeOpenPasteDialog returned false', function () {
+        describe('Prevent show dialog', function() {
+            it('Should not show paste html dialog if beforeOpenPasteDialog returned false', function() {
                 var editor = new Jodit(appendTestArea(), {
                     events: {
-                        beforeOpenPasteDialog: function (msg, title, callback, clearButton, clear2Button) {
+                        beforeOpenPasteDialog: function(
+                            msg,
+                            title,
+                            callback,
+                            clearButton,
+                            clear2Button
+                        ) {
                             return false;
-                        }
-                    }
+                        },
+                    },
                 });
 
                 var pastedText = '<p>test</p>';
 
-                var emulatePasteEvent = function (data) {
+                var emulatePasteEvent = function(data) {
                     data.clipboardData = {
                         types: ['text/html'],
-                        getData: function (type) {
+                        getData: function(type) {
                             return pastedText;
-                        }
+                        },
                     };
                 };
 
@@ -48,27 +58,38 @@ describe('Clipboard text', function () {
 
                 expect(editor.value).to.be.equal('');
 
-                var dialog = editor.ownerDocument.querySelector('.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id='  + editor.id + ']')
+                var dialog = editor.ownerDocument.querySelector(
+                    '.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id=' +
+                        editor.id +
+                        ']'
+                );
                 expect(dialog).to.be.equal(null);
             });
-            describe('Change dialog in afterOpenPasteDialog', function () {
-                it('Should change dialog', function () {
+            describe('Change dialog in afterOpenPasteDialog', function() {
+                it('Should change dialog', function() {
                     var editor = new Jodit(appendTestArea(), {
                         events: {
-                            afterOpenPasteDialog: function (dialog, msg, title, callback, clearButton, clear2Button) {
+                            afterOpenPasteDialog: function(
+                                dialog,
+                                msg,
+                                title,
+                                callback,
+                                clearButton,
+                                clear2Button
+                            ) {
                                 dialog.container.style.left = '10px';
-                            }
-                        }
+                            },
+                        },
                     });
 
                     var pastedText = '<p>test</p>';
 
-                    var emulatePasteEvent = function (data) {
+                    var emulatePasteEvent = function(data) {
                         data.clipboardData = {
                             types: ['text/html'],
-                            getData: function (type) {
+                            getData: function(type) {
                                 return pastedText;
-                            }
+                            },
                         };
                     };
 
@@ -76,27 +97,31 @@ describe('Clipboard text', function () {
 
                     expect(editor.value).to.be.equal('');
 
-                    var dialog = editor.ownerDocument.querySelector('.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id='  + editor.id + ']')
+                    var dialog = editor.ownerDocument.querySelector(
+                        '.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id=' +
+                            editor.id +
+                            ']'
+                    );
                     expect(dialog).to.be.not.equal(null);
                     expect(parseInt(dialog.style.left, 10)).to.be.equal(10);
                 });
             });
         });
     });
-    describe('Paste simple text', function () {
-        it('Should not show paste html dialog', function () {
+    describe('Paste simple text', function() {
+        it('Should not show paste html dialog', function() {
             var editor = new Jodit(appendTestArea(), {
                 defaultActionOnPaste: Jodit.INSERT_AS_HTML,
             });
 
             var pastedText = 'test';
 
-            var emulatePasteEvent = function (data) {
+            var emulatePasteEvent = function(data) {
                 data.clipboardData = {
                     types: ['text/html'],
-                    getData: function (type) {
+                    getData: function(type) {
                         return pastedText;
-                    }
+                    },
                 };
             };
 
@@ -104,13 +129,17 @@ describe('Clipboard text', function () {
 
             expect(editor.value).to.be.equal('test');
 
-            var dialog = editor.ownerDocument.querySelector('.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id='  + editor.id + ']')
+            var dialog = editor.ownerDocument.querySelector(
+                '.jodit.jodit_dialog_box.active.jodit_modal[data-editor_id=' +
+                    editor.id +
+                    ']'
+            );
             expect(dialog).to.be.equal(null);
         });
     });
-    describe('Paste text', function () {
-        describe('Insert only text', function () {
-            it('Should insert only text from pasted html', function () {
+    describe('Paste text', function() {
+        describe('Insert only text', function() {
+            it('Should insert only text from pasted html', function() {
                 var editor = new Jodit(appendTestArea(), {
                     askBeforePasteHTML: false,
                     askBeforePasteFromWord: false,
@@ -118,12 +147,12 @@ describe('Clipboard text', function () {
                 });
                 var pastedText = '<p>test</p>';
 
-                var emulatePasteEvent = function (data) {
+                var emulatePasteEvent = function(data) {
                     data.clipboardData = {
                         types: ['text/html'],
-                        getData: function (type) {
+                        getData: function(type) {
                             return pastedText;
-                        }
+                        },
                     };
                 };
 
@@ -131,8 +160,8 @@ describe('Clipboard text', function () {
                 expect(editor.value).to.be.equal('test');
             });
         });
-        describe('Insert as text', function () {
-            it('Should insert only text from pasted html', function () {
+        describe('Insert as text', function() {
+            it('Should insert only text from pasted html', function() {
                 var editor = new Jodit(appendTestArea(), {
                     askBeforePasteHTML: false,
                     askBeforePasteFromWord: false,
@@ -140,12 +169,12 @@ describe('Clipboard text', function () {
                 });
                 var pastedText = '<p>test</p>';
 
-                var emulatePasteEvent = function (data) {
+                var emulatePasteEvent = function(data) {
                     data.clipboardData = {
                         types: ['text/html'],
-                        getData: function (type) {
+                        getData: function(type) {
                             return pastedText;
-                        }
+                        },
                     };
                 };
 
@@ -153,8 +182,8 @@ describe('Clipboard text', function () {
                 expect(editor.value).to.be.equal('&lt;p&gt;test&lt;/p&gt;');
             });
         });
-        describe('Insert as html', function () {
-            it('Should insert pasted html like html', function () {
+        describe('Insert as html', function() {
+            it('Should insert pasted html like html', function() {
                 var editor = new Jodit(appendTestArea(), {
                     askBeforePasteHTML: false,
                     askBeforePasteFromWord: false,
@@ -163,12 +192,12 @@ describe('Clipboard text', function () {
 
                 var pastedText = '<p>test</p>';
 
-                var emulatePasteEvent = function (data) {
+                var emulatePasteEvent = function(data) {
                     data.clipboardData = {
                         types: ['text/html'],
-                        getData: function (type) {
+                        getData: function(type) {
                             return pastedText;
-                        }
+                        },
                     };
                 };
 
@@ -176,8 +205,8 @@ describe('Clipboard text', function () {
                 expect(editor.value).to.be.equal('<p>test</p><br>');
             });
         });
-        describe('Insert clear html', function () {
-            it('Should insert pasted and cleared html', function () {
+        describe('Insert clear html', function() {
+            it('Should insert pasted and cleared html', function() {
                 var editor = new Jodit(appendTestArea(), {
                     askBeforePasteHTML: false,
                     askBeforePasteFromWord: false,
@@ -186,12 +215,12 @@ describe('Clipboard text', function () {
 
                 var pastedText = '<p style="color:red;" data-text="1">test</p>';
 
-                var emulatePasteEvent = function (data) {
+                var emulatePasteEvent = function(data) {
                     data.clipboardData = {
                         types: ['text/html'],
-                        getData: function (type) {
+                        getData: function(type) {
                             return pastedText;
-                        }
+                        },
                     };
                 };
 
