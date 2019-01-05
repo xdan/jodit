28edb9d03982183ac08622d1d07118a621b3a205
Valera <v-chupurnov@edadeal.ru>
2019-01-05 18:42:17 +0500

    Changed copyright years

--- src/modules/toolbar/collection.ts
+++ src/modules/toolbar/collection.ts
@@ -1,7 +1,7 @@
 /*!
  * Jodit Editor (https://xdsoft.net/jodit/)
  * License GNU General Public License version 2 or later;
- * Copyright 2013-2018 Valeriy Chupurnov https://xdsoft.net
+ * Copyright 2013-2019 Valeriy Chupurnov https://xdsoft.net
  */
 
 import { IDictionary, IToolbarCollection } from '../../types';
