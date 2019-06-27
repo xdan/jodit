54c5e869585d23ef1f45de956bd662e2223388f8
Valera <v-chupurnov@edadeal.ru>
2019-06-27 22:21:48 +0300

    Added information about commercial license

--- src/modules/Dom.ts
+++ src/modules/Dom.ts
@@ -1,6 +1,6 @@
 /*!
  * Jodit Editor (https://xdsoft.net/jodit/)
- * License GNU General Public License version 2 or later;
+ * Licensed under GNU General Public License version 2 or later or a commercial license;
  * Copyright 2013-2019 Valeriy Chupurnov https://xdsoft.net
  */
 
--- src/modules/helpers/checker/isPlainObject.ts
+++ src/modules/helpers/checker/isPlainObject.ts
@@ -1,7 +1,9 @@
 /*!
  * Jodit Editor (https://xdsoft.net/jodit/)
- * License GNU General Public License version 2 or later;
- * Copyright 2013-2019 Valeriy Chupurnov https://xdsoft.net
+ * Licensed under GNU General Public License version 2 or later or a commercial license;
+ * For GPL see LICENSE.txt in the project root for license information.
+ * For commercial licenses see https://xdsoft.net/jodit/commercial/
+ * Copyright (c) 2013-2019 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
  */
 
 import { isWindow } from './isWindow';
