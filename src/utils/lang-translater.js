f3ece67a1a162f00d02b18bb357e78b1b6dbf4e7
Valera <skoder@ya.ru>
2019-11-25 22:53:37 +0300

    Added translate script helper

--- src/utils/lang-translater.js
+++ src/utils/lang-translater.js
@@ -1,7 +1,13 @@
 const https = require('https');
 
-const yandex_translate_api_key  =
-	'';
+const yandex_translate_api_key  = process.argv[2];
+
+if (!yandex_translate_api_key) {
+	throw new Error(
+		'Need Yandex Translate API key. https://translate.yandex.com/developers/keys\n' +
+		'You can run it file: node ./src/utils/lang-translater.js %api_key%'
+	);
+}
 
 const translate = async (text, lang) => {
 	return new Promise((resolve, reject) => {
@@ -36,15 +42,31 @@ const path = require('path');
 const folder = path.resolve(__dirname, '../langs');
 const files = fs.readdirSync(folder);
 
+const replace = {
+	cs_cz: 'cs',
+	zh_cn: 'zh',
+	zh_tw: 'th',
+	pt_br: 'pt',
+};
+
 const translateAll = (text) => {
 	files.forEach(async (file) => {
 		const filename = path.join(folder, file);
+		let lang = file.replace(/\.ts$/, '');
+
+		if (['index', 'en'].includes(lang)) {
+			return;
+		}
+
+		if (replace[lang]) {
+			lang = replace[lang];
+		}
+
 		const data = fs.readFileSync(filename, 'utf-8');
 
 		const end = data.indexOf('} as IDictionary<string>;');
 
 		if (end !== -1) {
-			const lang = file.replace(/\.ts$/, '');
 			const translated = await translate(text, lang);
 			const newHash = data.substring(0, end - 1) + "\n\t'" + text + "': '" + translated + "',\n" + data.substring(end);
 
