3455c363308af2940a58f830e3a44cdd84890489
Valera <skoder@ya.ru>
2020-01-07 01:01:58 +0500

    All files must have copyright

--- src/plugins/source/editors/ace.ts
+++ src/plugins/source/editors/ace.ts
@@ -1,3 +1,12 @@
+/*!
+ * Jodit Editor (https://xdsoft.net/jodit/)
+ * Licensed under GNU General Public License version 2 or later or a commercial license or MIT;
+ * For GPL see LICENSE-GPL.txt in the project root for license information.
+ * For MIT see LICENSE-MIT.txt in the project root for license information.
+ * For commercial licenses see https://xdsoft.net/jodit/commercial/
+ * Copyright (c) 2013-2019 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
+ */
+
 import { IJodit, ISourceEditor } from '../../../types';
 import * as consts from '../../../constants';
 import { $$, isString, loadNext } from '../../../modules/helpers';
