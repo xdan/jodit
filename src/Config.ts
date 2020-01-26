2a2831cc51861ac8b820a4f12bc8c456d0950029
Valera <skoder@ya.ru>
2020-01-27 00:44:45 +0300

    Remove GPL license

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 /*!
  * Jodit Editor (https://xdsoft.net/jodit/)
  * Licensed under GNU General Public License version 2 or later or a commercial license or MIT;
- * For MIT see LICENSE-MIT.txt in the project root for license information.
+ * Released under MIT see LICENSE.txt in the project root for license information.
   * Copyright (c) 2013-2020 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
  */
 
