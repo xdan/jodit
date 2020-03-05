9fd479e94d6bd1c270db46cc8672452756d4b662
Rare Kang <rare@soomgo.com>
2020-03-06 04:21:33 +0900

    Fix AceAjax references to tsconfig

--- src/plugins/source/editor/engines/ace.ts
+++ src/plugins/source/editor/engines/ace.ts
@@ -4,7 +4,6 @@
  * Copyright (c) 2013-2020 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
  */
 
-import '@types/ace';
 import { IJodit, ISourceEditor } from '../../../../types';
 import * as consts from '../../../../constants';
 import { isString, loadNext } from '../../../../modules/helpers';
