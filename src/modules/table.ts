a8d64f45eb3595622a772f3e33f5e9dc9b2f56c4
Dave van Rijn <develop@davevanrijn.nl>
2020-07-07 21:58:07 +0200

    Trying to fix last bug

--- src/modules/table.ts
+++ src/modules/table.ts
@@ -41,7 +41,7 @@ export class Table extends ViewComponent<IJodit> {
 
 		// eslint-disable-next-line no-prototype-builtins
 		style.innerHTML = selectors.length
-			? selectors.join(',') + `{${(this.jodit.options as Config).table.selectionCellStyle}}`
+			? selectors.join(',') + `{${(this.jodit.options as any).table.selectionCellStyle}}`
 			: '';
 	}
 
