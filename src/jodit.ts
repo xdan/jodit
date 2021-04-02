eed20863aadfd3dfc574c2084408a30e8e2e40c3
SCAREX <scarex.contact@gmail.com>
2021-04-02 15:42:21 +0200

    Fixed spellcheck set to false
    When spellcheck is not added the default value is browser defined, in most of them the default value is true.

--- src/jodit.ts
+++ src/jodit.ts
@@ -1434,6 +1434,8 @@ export class Jodit extends ViewWithToolbar implements IJodit {
 
 		if (this.o.spellcheck) {
 			this.editor.setAttribute('spellcheck', 'true');
+		} else {
+			this.editor.setAttribute('spellcheck', 'false');
 		}
 
 		// direction
