46cb734ff520e4814ede2329fe8b72e2a146debe
xdan <skoder@ya.ru>
2021-11-24 01:48:01 +0300

    Added command `preview`

--- src/plugins/print/preview.ts
+++ src/plugins/print/preview.ts
@@ -6,65 +6,12 @@
 
 import type { IControlType, IDialog, IJodit } from '../../types';
 import { Config } from '../../config';
-import { css } from '../../core/helpers';
+import { attr, css } from '../../core/helpers';
 import * as consts from '../../core/constants';
 
 Config.prototype.controls.preview = {
 	icon: 'eye',
-	exec: (editor: IJodit) => {
-		const dialog = <IDialog>editor.getInstance('Dialog', {
-			language: editor.o.language,
-			theme: editor.o.theme
-		});
-
-		const div = editor.c.div();
-		css(div, {
-			position: 'relative',
-			padding: 16
-		});
-
-		const value =
-			editor.value ||
-			`<div style='position: absolute;left:50%;top:50%;transform: translateX(-50%) translateY(-50%);color:#ccc;'>${editor.i18n(
-				'Empty'
-			)}</div>`;
-
-		if (editor.iframe) {
-			const iframe = editor.create.element('iframe');
-
-			css(iframe, {
-				minWidth: 800,
-				minHeight: 600,
-				border: 0
-			});
-
-			div.appendChild(iframe);
-			dialog.open(div, editor.i18n('Preview'));
-
-			const mywindow = iframe.contentWindow;
-
-			if (mywindow) {
-				editor.e.fire(
-					'generateDocumentStructure.iframe',
-					mywindow.document,
-					editor
-				);
-
-				mywindow.document.body.innerHTML = value;
-			}
-		} else {
-			css(div, {
-				minWidth: 1024,
-				minHeight: 600,
-				border: 0
-			});
-
-			div.innerHTML = value;
-			dialog.open(div, editor.i18n('Preview'));
-		}
-
-		dialog.setModal(true);
-	},
+	command: 'preview',
 	mode: consts.MODE_SOURCE + consts.MODE_WYSIWYG,
 	tooltip: 'Preview'
 } as IControlType;
@@ -73,4 +20,105 @@ export function preview(editor: IJodit): void {
 	editor.registerButton({
 		name: 'preview'
 	});
+
+	editor.registerCommand(
+		'preview',
+		(_: any, _1: any, defaultValue: string) => {
+			const dialog = <IDialog>editor.getInstance('Dialog', {
+				language: editor.o.language,
+				theme: editor.o.theme
+			});
+
+			const div = editor.c.div();
+			css(div, {
+				position: 'relative',
+				padding: 16
+			});
+
+			const value =
+				defaultValue ||
+				editor.value ||
+				`<div style='position: absolute;left:50%;top:50%;transform: translateX(-50%) translateY(-50%);color:#ccc;'>${editor.i18n(
+					'Empty'
+				)}</div>`;
+
+			if (editor.iframe) {
+				const iframe = editor.create.element('iframe');
+
+				css(iframe, {
+					minWidth: 800,
+					minHeight: 600,
+					border: 0
+				});
+
+				div.appendChild(iframe);
+				dialog.open(div, editor.i18n('Preview'));
+
+				const mywindow = iframe.contentWindow;
+
+				if (mywindow) {
+					editor.e.fire(
+						'generateDocumentStructure.iframe',
+						mywindow.document,
+						editor
+					);
+
+					mywindow.document.body.innerHTML = value;
+				}
+			} else {
+				css(div, {
+					minWidth: 1024,
+					minHeight: 600,
+					border: 0
+				});
+
+				dialog.setSize(1024, 600).open(div, editor.i18n('Preview'));
+
+				const setHTML = (box: HTMLElement, value: string): void => {
+					const dv = editor.c.div();
+					dv.innerHTML = value;
+
+					for (let i = 0; i < dv.children.length; i += 1) {
+						const c = dv.children[i];
+
+						const newNode = document.createElement(c.nodeName);
+
+						for (let j = 0; j < c.attributes.length; j += 1) {
+							attr(
+								newNode,
+								c.attributes[j].nodeName,
+								c.attributes[j].nodeValue
+							);
+						}
+
+						if (c.children.length === 0) {
+							switch (c.nodeName) {
+								case 'SCRIPT':
+									if (c.textContent) {
+										newNode.textContent = c.textContent;
+									}
+									break;
+
+								default:
+									if (c.innerHTML) {
+										newNode.innerHTML = c.innerHTML;
+									}
+									break;
+							}
+						} else {
+							setHTML(newNode, c.innerHTML);
+						}
+
+						try {
+							box.appendChild(newNode);
+						} catch {}
+					}
+				};
+
+				setHTML(div, value);
+			}
+
+			dialog.setModal(true);
+		}
+	);
 }
