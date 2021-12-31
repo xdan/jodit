2be1128100b3231350448ce1e4b5ce0e334129d1
xdan <skoder@ya.ru>
2021-12-31 05:38:55 +0300

    Rename observe to observer

---
+++ src/types/observer.d.ts
@@ -0,0 +1,48 @@
+/*!
+ * Jodit Editor (https://xdsoft.net/jodit/)
+ * Released under MIT see LICENSE.txt in the project root for license information.
+ * Copyright (c) 2013-2021 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
+ */
+
+/**
+ * @module types
+ */
+
+import { CallbackFunction, CanUndef, IComponent, IDictionary, SnapshotType } from './types';
+
+export interface IObservable {
+	on(event: string | string[], callback: CallbackFunction): this;
+	fire(event: string | string[], ...attr: any[]): void;
+}
+
+export interface ICommand {
+	undo(): void;
+	redo(): void;
+}
+
+export interface IStack {
+	clear(): void;
+	push(command: ICommand): void;
+	replace(command: ICommand): void;
+	current(): CanUndef<ICommand>;
+	undo(): boolean;
+	redo(): boolean;
+	canUndo(): boolean;
+	canRedo(): boolean;
+}
+
+export interface ISnapshot {
+	make(): SnapshotType;
+	restoreOnlySelection(snapshot: SnapshotType): void;
+	restore(snapshot: SnapshotType): void;
+}
+
+export interface IObserver extends IComponent {
+	stack: IStack;
+	snapshot: ISnapshot;
+	redo(): void;
+	undo(): void;
+	clear(): void;
+	replaceSnapshot(): void;
+	upTick(): void;
+}
