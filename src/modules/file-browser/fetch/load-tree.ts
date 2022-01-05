265eef357d2bb698fa8339b45a3f69c7792b0253
xdan <skoder@ya.ru>
2022-01-06 01:26:16 +0300

    Fixed docs

--- src/modules/file-browser/fetch/load-tree.ts
+++ src/modules/file-browser/fetch/load-tree.ts
@@ -3,11 +3,19 @@
  * Released under MIT see LICENSE.txt in the project root for license information.
  * Copyright (c) 2013-2022 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
  */
+
+/**
+ * @module modules/file-browser
+ */
+
 import type { IFileBrowser } from '../../../types';
 import { error } from '../../../core/helpers/utils/error';
 import { Dom } from '../../../core/dom';
 import { loadItems } from './load-items';
 
+/**
+ * Loads a list of directories
+ */
 export async function loadTree(fb: IFileBrowser): Promise<any> {
 	const errorUni = (e: string | Error) => {
 		throw e instanceof Error ? e : error(e);
