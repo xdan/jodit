bdd2907b28af10a00567edfb2e4a3f6d03e86e28
xdan <skoder@ya.ru>
2022-01-19 23:20:52 +0300

    Fixed docs

--- src/core/helpers/utils/assert.ts
+++ src/core/helpers/utils/assert.ts
@@ -4,6 +4,10 @@
  * Copyright (c) 2013-2022 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
  */
 
+/**
+ * @module helpers/utils
+ */
+
 class AssertionError extends Error {
 	constructor(message: string) {
 		super(message);
