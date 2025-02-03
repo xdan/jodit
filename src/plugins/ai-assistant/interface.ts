6557ae444e441e78e83d96c306b0b7af5e81fb9e
xdan <chupurnov@gmail.com>
2025-02-04 01:07:41 +0300

    New version 4.5.3 Read more https://github.com/xdan/jodit/blob/main/CHANGELOG.md

--- src/plugins/ai-assistant/interface.ts
+++ src/plugins/ai-assistant/interface.ts
@@ -1,3 +1,13 @@
+/*!
+ * Jodit Editor (https://xdsoft.net/jodit/)
+ * Released under MIT see LICENSE.txt in the project root for license information.
+ * Copyright (c) 2013-2025 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
+ */
+
+/**
+ * @module plugins/ai-assistant
+ */
+
 export interface AiAssistantSettings {
 	/** Callback function for AI assistant to process and return the result */
 	aiAssistantCallback?: (
