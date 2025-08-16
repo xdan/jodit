44f5d15dd0751fb8ea556565b2a656edb93419f6
paokgh <paoking230@gmail.com>
2025-08-16 16:53:14 -0400

    don't remove nodes with visible characters

--- src/plugins/clean-html/helpers/visitor/filters/try-remove-node.ts
+++ src/plugins/clean-html/helpers/visitor/filters/try-remove-node.ts
@@ -11,7 +11,7 @@
 import type { IDictionary, IJodit, Nullable } from 'jodit/types';
 import { IS_INLINE } from 'jodit/core/constants';
 import { Dom } from 'jodit/core/dom/dom';
-import { trim } from 'jodit/core/helpers/string/trim';
+import { trimInv } from 'jodit/core/helpers/string/trim';
 
 /**
  * @private
@@ -68,7 +68,7 @@ function isRemovableNode(
 		Dom.isElement(node) &&
 		node.nodeName.match(IS_INLINE) != null &&
 		!Dom.isTemporary(node) &&
-		trim((node as Element).innerHTML).length === 0 &&
+		trimInv((node as Element).innerHTML).length === 0 &&
 		(current == null || !Dom.isOrContains(node, current))
 	);
 }
