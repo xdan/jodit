24f251e67692ad8445589909c66d638bcf2dfd42
xdan <chupurnov@gmail.com>
2026-01-30 13:39:51 +0300

    fix: correct spelling of "Valeriy" to "Valerii" in copyright notices across multiple files

--- src/langs/zh_tw.js
+++ src/langs/zh_tw.js
@@ -1,7 +1,7 @@
 /*!
  * Jodit Editor (https://xdsoft.net/jodit/)
  * Released under MIT see LICENSE.txt in the project root for license information.
- * Copyright (c) 2013-2026 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
+ * Copyright (c) 2013-2026 Valerii Chupurnov. All rights reserved. https://xdsoft.net
  */
 
 // Chinese Traditional (繁體中文)
